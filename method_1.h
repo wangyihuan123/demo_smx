a4f0a2cb13b4d66441dbe863afb7a8681beca3eb
Benjamin.Wang <wangyihuan123@hotmail.com>
2017-03-09 23:35:40 +1300

    remove old files and update the code

---
+++ method_1.h
@@ -0,0 +1,14 @@
+
+#ifndef DEMO_SMX_METHOD_1_H
+#define DEMO_SMX_METHOD_1_H
+
+#include "state_machine.h"
+#include "lib.h"
+
+extern void
+SM_first_state_2_action (state_machine_t* this);
+
+extern Bool
+SM_first_check_conditions (state_machine_t* this);
+
+#endif //DEMO_SMX_METHOD_1_H
